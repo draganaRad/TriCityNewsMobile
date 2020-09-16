af1f88f6c0006e37efa444ed569ac9587f5a7f53
Dragana <dragana1010@yahoo.ca>
2020-09-16 14:47:04 -0700

    testing images

--- maps/js/utils.js
+++ maps/js/utils.js
@@ -1,11 +1,11 @@
 // map line color and style for all news items
-var lineStyle = {
+let lineStyle = {
 		"color": "#40AEE0", // iPhone app "AccentColor"
 		"weight": 7,
 		"opacity": 0.8
 };
 
-var tileUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
-var tileAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-var tileSubdomain = 'abcd'
+let tileUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+let tileAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+let tileSubdomain = 'abcd'
 
