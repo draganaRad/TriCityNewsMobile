a8ceae47c01180c8c42551be7b50a3b3d6cc6fb6
Dragana <dragana1010@yahoo.ca>
2020-09-16 13:03:40 -0700

    cleaned up PM_skyTrain item

---
+++ maps/data/PM_skyTrainData.js
@@ -0,0 +1,77 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-09-15T20:41:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/834794798",
+        "foot": "yes",
+        "highway": "cycleway",
+        "oneway": "no",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8500501,
+            49.277812
+          ],
+          [
+            -122.8499804,
+            49.2778119
+          ],
+          [
+            -122.8499495,
+            49.2777752
+          ],
+          [
+            -122.8499455,
+            49.2777516
+          ],
+          [
+            -122.8499629,
+            49.2777113
+          ],
+          [
+            -122.8499562,
+            49.2776851
+          ],
+          [
+            -122.8499187,
+            49.2776728
+          ],
+          [
+            -122.8498557,
+            49.2776798
+          ],
+          [
+            -122.8498235,
+            49.2776991
+          ],
+          [
+            -122.8498221,
+            49.2777419
+          ],
+          [
+            -122.8498275,
+            49.2777804
+          ],
+          [
+            -122.8498288,
+            49.2778076
+          ],
+          [
+            -122.849816,
+            49.2778281
+          ]
+        ]
+      },
+      "id": "way/834794798"
+    }
+  ]
+}
---
+++ maps/js/utils.js
@@ -0,0 +1,11 @@
+// map line color and style for all news items
+var lineStyle = {
+		"color": "#FF7F00", // darkOrange_color ("Paired")
+		"weight": 5,
+		"opacity": 0.5
+};
+
+var tileUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+var tileAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+var tileSubdomain = 'abcd'
+
