62ab983d302627930b5fa7849c5ae7b1ec15b067
Dragana <dragana1010@yahoo.ca>
2020-09-16 21:49:34 -0700

    added more items and images

--- maps/js/utils.js
+++ maps/js/utils.js
@@ -5,6 +5,12 @@ let lineStyle = {
 		"opacity": 0.8
 };
 
+let lowStressStyle = {
+	"color": "#377EB8", // Stress map blue (Mapbox)
+	"weight": 7,
+	"opacity": 0.8
+};
+
 let tileUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
 let tileAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 let tileSubdomain = 'abcd'
