a921914349a337c0eb222d3b8fa99316de3f785e
Dragana <dragana1010@yahoo.ca>
2020-09-21 14:53:55 -0700

    added Murray St and Sheep Paddocks

---
+++ maps/data/PM_murrayStMUP.js
@@ -0,0 +1,130 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-09-21T21:32:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/834817465",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "path",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8499257,
+            49.2789332
+          ],
+          [
+            -122.8486814,
+            49.2789392
+          ],
+          [
+            -122.848575,
+            49.2789392
+          ],
+          [
+            -122.848438,
+            49.2789368
+          ],
+          [
+            -122.847847,
+            49.2789383
+          ],
+          [
+            -122.8475828,
+            49.2789394
+          ],
+          [
+            -122.8474781,
+            49.2789613
+          ]
+        ]
+      },
+      "id": "way/834817465"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/839155746",
+        "footway": "crossing",
+        "highway": "footway"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8499252,
+            49.2787791
+          ],
+          [
+            -122.8499254,
+            49.278857
+          ],
+          [
+            -122.8499257,
+            49.2789332
+          ]
+        ]
+      },
+      "id": "way/839155746"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/834816257",
+        "footway": "sidewalk",
+        "highway": "footway",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8525796,
+            49.2789182
+          ],
+          [
+            -122.8525782,
+            49.2788731
+          ],
+          [
+            -122.852575,
+            49.2787715
+          ],
+          [
+            -122.8524258,
+            49.2787709
+          ],
+          [
+            -122.851497,
+            49.278767
+          ],
+          [
+            -122.850746,
+            49.2787655
+          ],
+          [
+            -122.8503287,
+            49.2787655
+          ],
+          [
+            -122.850025,
+            49.2787806
+          ],
+          [
+            -122.8499252,
+            49.2787791
+          ]
+        ]
+      },
+      "id": "way/834816257"
+    }
+  ]
+}
