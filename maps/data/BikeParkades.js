b45c96187f6b14c3e867b755275d38c938bf4e5f
Dragana <dragana1010@yahoo.ca>
2020-09-21 13:34:25 -0700

    added few more maps

---
+++ maps/data/BikeParkades.js
@@ -0,0 +1,56 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-09-18T23:13:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4587023756",
+        "crossing": "marked",
+        "highway": "crossing"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -122.774370,
+            49.261776
+        ]
+      },
+      "id": "node/4587023756"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/7763952495",
+        "crossing": "marked",
+        "highway": "crossing"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -122.890275,
+            49.261122
+        ]
+      },
+      "id": "node/7763952495"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/7763952496",
+        "crossing": "marked",
+        "highway": "crossing"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+            -122.791667,
+            49.285556
+        ]
+      },
+      "id": "node/7763952496"
+    }
+  ]
+}
