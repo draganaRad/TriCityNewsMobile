fe167ed7937046bbe5253e7be1c43d3b2639824c
Dragana <dragana1010@yahoo.ca>
2020-11-13 15:51:32 -0800

    added new loughedd MUP files

---
+++ maps/data/C_lougheedMUP2.js
@@ -0,0 +1,65 @@
+var geojsonFeature = {
+    "type": "FeatureCollection",
+    "generator": "overpass-ide",
+    "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+    "timestamp": "2020-11-13T23:21:02Z",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "@id": "way/67978162",
+          "bicycle": "designated",
+          "foot": "designated",
+          "highway": "path",
+          "segregated": "no",
+          "surface": "asphalt"
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              -122.7989678,
+              49.2674961
+            ],
+            [
+              -122.7989499,
+              49.2673735
+            ],
+            [
+              -122.7989469,
+              49.267211
+            ],
+            [
+              -122.7989017,
+              49.266178
+            ],
+            [
+              -122.7987852,
+              49.2654113
+            ],
+            [
+              -122.7986816,
+              49.2647248
+            ],
+            [
+              -122.7986234,
+              49.2643806
+            ],
+            [
+              -122.7985513,
+              49.2639382
+            ],
+            [
+              -122.7984479,
+              49.263321
+            ],
+            [
+              -122.7984656,
+              49.263253
+            ]
+          ]
+        },
+        "id": "way/67978162"
+      }
+    ]
+  }
