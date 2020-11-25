e5f5794f6086b626e725c3539c92bf9fb4ade4ca
Dragana <dragana1010@yahoo.ca>
2020-11-24 17:23:40 -0800

    added Como Creek bridge

---
+++ maps/data/C_comoCreekBridge.js
@@ -0,0 +1,35 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-11-25T01:01:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/877294119",
+        "bicycle": "designated",
+        "bridge": "yes",
+        "foot": "designated",
+        "highway": "path",
+        "layer": "1",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8559766,
+            49.2514767
+          ],
+          [
+            -122.8553584,
+            49.251475
+          ]
+        ]
+      },
+      "id": "way/877294119"
+    }
+  ]
+}
