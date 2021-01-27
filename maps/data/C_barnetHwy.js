37d2f1cea088be6ad342ba16b9f30593ac6a0ef7
Dragana Radulovic <draganaradulovic@Draganas-MBP.vc.shawcable.net>
2021-01-26 19:45:18 -0800

    added untracked files

---
+++ maps/data/C_barnetHwy.js
@@ -0,0 +1,125 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2021-01-27T02:56:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/900534997",
+        "bicycle": "designated",
+        "cycleway:surface": "asphalt",
+        "foot": "designated",
+        "footway:surface": "concrete",
+        "highway": "path",
+        "lit": "yes",
+        "segregated": "yes",
+        "surface": "paved",
+        "width": "3"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7991979,
+            49.2769892
+          ],
+          [
+            -122.7990047,
+            49.2769402
+          ],
+          [
+            -122.7984308,
+            49.2768195
+          ],
+          [
+            -122.7981558,
+            49.2767372
+          ],
+          [
+            -122.7978206,
+            49.2766147
+          ],
+          [
+            -122.7976113,
+            49.2765806
+          ],
+          [
+            -122.7974249,
+            49.2765745
+          ],
+          [
+            -122.7973683,
+            49.2765897
+          ],
+          [
+            -122.7972579,
+            49.2767013
+          ]
+        ]
+      },
+      "id": "way/900534997"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/900534999",
+        "bicycle": "yes",
+        "crossing": "marked",
+        "footway": "crossing",
+        "highway": "footway",
+        "segregated": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7991979,
+            49.2769892
+          ],
+          [
+            -122.7993396,
+            49.2770211
+          ],
+          [
+            -122.7994502,
+            49.2770458
+          ]
+        ]
+      },
+      "id": "way/900534999"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/900535000",
+        "bicycle": "designated",
+        "cycleway:surface": "asphalt",
+        "foot": "designated",
+        "footway:surface": "concrete",
+        "highway": "path",
+        "segregated": "yes",
+        "surface": "paved"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7994502,
+            49.2770458
+          ],
+          [
+            -122.7995551,
+            49.2770515
+          ],
+          [
+            -122.7996687,
+            49.2770724
+          ]
+        ]
+      },
+      "id": "way/900535000"
+    }
+  ]
+}
