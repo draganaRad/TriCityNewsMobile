0ce326e576886efd43043aac8c4ff17231cc7146
Dragana Radulovic <draganaradulovic@Draganas-MBP.vc.shawcable.net>
2021-03-08 17:28:50 -0800

    added temp slow zones on St George, Port Moody

---
+++ maps/data/PM_StGeorgeTemp.js
@@ -0,0 +1,56 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.84786581993102,
+            49.27562385395029
+          ],
+          [
+            -122.84017324447633,
+            49.275567856184445
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.85245776176451,
+            49.27565185280935
+          ],
+          [
+            -122.8566312789917,
+            49.27565885252162
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.86173820495605,
+            49.275630853666534
+          ],
+          [
+            -122.86678075790405,
+            49.275567856184445
+          ]
+        ]
+      }
+    }
+  ]
+}
