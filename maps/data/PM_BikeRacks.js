d8d9d23e761be920b530a1df6588cbdf9b709e40
Dragana Radulovic <draganaradulovic@Draganas-MBP.vc.shawcable.net>
2021-07-22 14:37:21 -0700

    added new files for Moody Station bike storage

---
+++ maps/data/PM_BikeRacks.js
@@ -0,0 +1,24 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2020-09-18T23:13:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/4587023756",
+        "crossing": "marked",
+        "highway": "crossing"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.84603,
+          49.27784
+        ]
+      },
+      "id": "node/4587023756"
+    }
+  ]
+}
