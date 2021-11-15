b5e2362dab7fa08aa9a5751f9018802243636835
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2021-11-15 12:28:36 -0800

    added MUP between Westwood and Christmas

---
+++ maps/data/C_LougheedWestwood.js
@@ -0,0 +1,139 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2021-11-15T19:50:17Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/893977147",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "path",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7931836,
+            49.2744156
+          ],
+          [
+            -122.7937714,
+            49.274658
+          ],
+          [
+            -122.7939629,
+            49.2747147
+          ],
+          [
+            -122.7941977,
+            49.2748408
+          ],
+          [
+            -122.7942598,
+            49.2748483
+          ],
+          [
+            -122.7943716,
+            49.2748909
+          ],
+          [
+            -122.7944326,
+            49.2749308
+          ],
+          [
+            -122.7948556,
+            49.2751041
+          ],
+          [
+            -122.7950072,
+            49.2751944
+          ],
+          [
+            -122.7954136,
+            49.2753475
+          ],
+          [
+            -122.7954641,
+            49.2753432
+          ]
+        ]
+      },
+      "id": "way/893977147"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/893977169",
+        "bicycle": "designated",
+        "crossing": "marked",
+        "footway": "crossing",
+        "highway": "footway",
+        "segregated": "no"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7930263,
+            49.2743511
+          ],
+          [
+            -122.7931348,
+            49.2743956
+          ],
+          [
+            -122.7931836,
+            49.2744156
+          ]
+        ]
+      },
+      "id": "way/893977169"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/997804672",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "path",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7930263,
+            49.2743511
+          ],
+          [
+            -122.7929611,
+            49.2743162
+          ],
+          [
+            -122.7926237,
+            49.2741623
+          ],
+          [
+            -122.7922174,
+            49.2739976
+          ],
+          [
+            -122.7913211,
+            49.273617
+          ],
+          [
+            -122.7912225,
+            49.2736041
+          ]
+        ]
+      },
+      "id": "way/997804672"
+    }
+  ]
+}
