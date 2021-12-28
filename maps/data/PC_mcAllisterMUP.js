65e9dd95204363f857b9f6d871d059e469abe259
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2021-12-27 18:07:17 -0800

    added Prairie and McAllister MUPs in PoCo

---
+++ maps/data/PC_mcAllisterMUP.js
@@ -0,0 +1,22 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.78108954429626,
+            49.262725189694464
+          ],
+          [
+            -122.77837783098221,
+            49.26160317902522
+          ]
+        ]
+      }
+    }
+  ]
+}
