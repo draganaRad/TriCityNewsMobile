f602e056aa63e1d8339ee4d4ae77ca5180d62afc
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2023-09-21 14:50:17 -0700

    added new shared between pinetree and westwood

---
+++ maps/data/C_LougheedPinetree.js
@@ -0,0 +1,218 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2023-09-20T18:45:26Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/884523472",
+        "bicycle": "designated",
+        "footway": "crossing",
+        "highway": "footway",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7955794,
+            49.2758922
+          ],
+          [
+            -122.7957354,
+            49.2759417
+          ],
+          [
+            -122.7958481,
+            49.2759809
+          ]
+        ]
+      },
+      "id": "way/884523472"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/884523473",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "path",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7958481,
+            49.2759809
+          ],
+          [
+            -122.7965345,
+            49.2762548
+          ],
+          [
+            -122.7967648,
+            49.2763523
+          ],
+          [
+            -122.7968369,
+            49.2763957
+          ]
+        ]
+      },
+      "id": "way/884523473"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/884523485",
+        "bicycle": "designated",
+        "crossing": "marked",
+        "crossing:markings": "zebra",
+        "footway": "crossing",
+        "highway": "footway",
+        "segregated": "no"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7936344,
+            49.2750384
+          ],
+          [
+            -122.793511,
+            49.2749919
+          ],
+          [
+            -122.7934113,
+            49.2749555
+          ],
+          [
+            -122.7932902,
+            49.2749107
+          ],
+          [
+            -122.793218,
+            49.2748836
+          ]
+        ]
+      },
+      "id": "way/884523485"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/884523486",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "path",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7907251,
+            49.2738492
+          ],
+          [
+            -122.7908174,
+            49.2738241
+          ],
+          [
+            -122.7909224,
+            49.2738385
+          ],
+          [
+            -122.7910959,
+            49.2739113
+          ],
+          [
+            -122.7914387,
+            49.2740552
+          ],
+          [
+            -122.791822,
+            49.274216
+          ],
+          [
+            -122.7923585,
+            49.2744631
+          ],
+          [
+            -122.7929551,
+            49.2747328
+          ],
+          [
+            -122.7931524,
+            49.2748265
+          ],
+          [
+            -122.793218,
+            49.2748836
+          ]
+        ]
+      },
+      "id": "way/884523486"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1209497763",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "path",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.7955794,
+            49.2758922
+          ],
+          [
+            -122.7955132,
+            49.2758346
+          ],
+          [
+            -122.7951721,
+            49.2757086
+          ],
+          [
+            -122.7950465,
+            49.2756622
+          ],
+          [
+            -122.7946616,
+            49.27544
+          ],
+          [
+            -122.7941332,
+            49.2751924
+          ],
+          [
+            -122.7938556,
+            49.2750699
+          ],
+          [
+            -122.793751,
+            49.2750428
+          ],
+          [
+            -122.7936344,
+            49.2750384
+          ]
+        ]
+      },
+      "id": "way/1209497763"
+    }
+  ]
+}
