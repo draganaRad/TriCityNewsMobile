b21afa0172a428859625218ecff6ee33b6bb0a0e
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2023-11-20 16:10:16 -0800

    added new Guildford protected bike lane

---
+++ maps/data/C_Guildford.js
@@ -0,0 +1,905 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2023-11-20T23:45:01Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/35195635",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8211708,
+            49.284633
+          ],
+          [
+            -122.820777,
+            49.284718
+          ],
+          [
+            -122.8204899,
+            49.2847489
+          ],
+          [
+            -122.8201514,
+            49.2847641
+          ],
+          [
+            -122.8197474,
+            49.2847534
+          ],
+          [
+            -122.8193888,
+            49.2847148
+          ],
+          [
+            -122.8190799,
+            49.2846517
+          ],
+          [
+            -122.8188269,
+            49.2845836
+          ],
+          [
+            -122.8186318,
+            49.2845178
+          ]
+        ]
+      },
+      "id": "way/35195635"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/35198106",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "1",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8098045,
+            49.2844209
+          ],
+          [
+            -122.8103708,
+            49.2842693
+          ],
+          [
+            -122.8105438,
+            49.2842189
+          ],
+          [
+            -122.8110495,
+            49.2840405
+          ],
+          [
+            -122.8116547,
+            49.2838128
+          ],
+          [
+            -122.8119584,
+            49.2837128
+          ],
+          [
+            -122.8121615,
+            49.2836549
+          ],
+          [
+            -122.8123975,
+            49.2835961
+          ]
+        ]
+      },
+      "id": "way/35198106"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/35198382",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8129597,
+            49.2834877
+          ],
+          [
+            -122.8132575,
+            49.2834567
+          ],
+          [
+            -122.8136449,
+            49.2834218
+          ],
+          [
+            -122.8139653,
+            49.2834117
+          ],
+          [
+            -122.8142444,
+            49.283416
+          ],
+          [
+            -122.8145431,
+            49.2834268
+          ],
+          [
+            -122.8150113,
+            49.2834771
+          ],
+          [
+            -122.8151213,
+            49.2834916
+          ],
+          [
+            -122.8153031,
+            49.2835156
+          ],
+          [
+            -122.8156883,
+            49.2835971
+          ],
+          [
+            -122.8158745,
+            49.2836409
+          ],
+          [
+            -122.8161862,
+            49.2837283
+          ],
+          [
+            -122.816488,
+            49.2838321
+          ],
+          [
+            -122.818339,
+            49.2845344
+          ]
+        ]
+      },
+      "id": "way/35198382"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/76160130",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8018848,
+            49.2852227
+          ],
+          [
+            -122.8030084,
+            49.2851808
+          ],
+          [
+            -122.8044181,
+            49.2851224
+          ],
+          [
+            -122.8048953,
+            49.2850915
+          ],
+          [
+            -122.8059501,
+            49.2849913
+          ],
+          [
+            -122.8060975,
+            49.2849778
+          ],
+          [
+            -122.8064437,
+            49.2849401
+          ],
+          [
+            -122.8068215,
+            49.2848957
+          ],
+          [
+            -122.8071859,
+            49.2848463
+          ],
+          [
+            -122.8076701,
+            49.2847752
+          ],
+          [
+            -122.8080237,
+            49.2847286
+          ],
+          [
+            -122.8081221,
+            49.2847102
+          ],
+          [
+            -122.8090529,
+            49.2845696
+          ],
+          [
+            -122.8095441,
+            49.2844823
+          ]
+        ]
+      },
+      "id": "way/76160130"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/76160132",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.818421,
+            49.2844467
+          ],
+          [
+            -122.8166322,
+            49.2837551
+          ],
+          [
+            -122.8162165,
+            49.2836113
+          ],
+          [
+            -122.8158958,
+            49.2835243
+          ],
+          [
+            -122.8156592,
+            49.2834677
+          ],
+          [
+            -122.815422,
+            49.2834213
+          ],
+          [
+            -122.8153263,
+            49.2834072
+          ],
+          [
+            -122.8149178,
+            49.2833468
+          ],
+          [
+            -122.8145082,
+            49.2833132
+          ],
+          [
+            -122.8140419,
+            49.2832995
+          ],
+          [
+            -122.8136658,
+            49.2833101
+          ],
+          [
+            -122.8134182,
+            49.2833229
+          ],
+          [
+            -122.8129194,
+            49.2833852
+          ]
+        ]
+      },
+      "id": "way/76160132"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/76160133",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8185477,
+            49.2846147
+          ],
+          [
+            -122.8188483,
+            49.2847139
+          ],
+          [
+            -122.8191312,
+            49.2847846
+          ],
+          [
+            -122.8194894,
+            49.2848434
+          ],
+          [
+            -122.8198288,
+            49.2848733
+          ],
+          [
+            -122.8201734,
+            49.2848792
+          ],
+          [
+            -122.8205169,
+            49.2848565
+          ],
+          [
+            -122.8208131,
+            49.2848163
+          ],
+          [
+            -122.820996,
+            49.2847797
+          ],
+          [
+            -122.821171,
+            49.2847399
+          ]
+        ]
+      },
+      "id": "way/76160133"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/305606784",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8094825,
+            49.2843712
+          ],
+          [
+            -122.8090127,
+            49.284462
+          ],
+          [
+            -122.8085292,
+            49.2845353
+          ],
+          [
+            -122.8076572,
+            49.2846629
+          ],
+          [
+            -122.8068118,
+            49.284783
+          ],
+          [
+            -122.8060087,
+            49.2848706
+          ],
+          [
+            -122.8048794,
+            49.2849755
+          ],
+          [
+            -122.8043978,
+            49.2850087
+          ],
+          [
+            -122.8029714,
+            49.2850707
+          ],
+          [
+            -122.8019064,
+            49.2851146
+          ],
+          [
+            -122.8018755,
+            49.2851158
+          ]
+        ]
+      },
+      "id": "way/305606784"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/494682487",
+        "attribution": "GeoBase®",
+        "cycleway:right": "lane",
+        "geobase:acquisitionTechnique": "GPS",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "source": "Geobase_Import_2009"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8128072,
+            49.283403
+          ],
+          [
+            -122.8126887,
+            49.2834218
+          ]
+        ]
+      },
+      "id": "way/494682487"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/494682488",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "1",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8123975,
+            49.2835961
+          ],
+          [
+            -122.8127365,
+            49.2835244
+          ]
+        ]
+      },
+      "id": "way/494682488"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/494682490",
+        "cycleway:right": "track",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8126887,
+            49.2834218
+          ],
+          [
+            -122.8123721,
+            49.2834839
+          ],
+          [
+            -122.8120959,
+            49.2835504
+          ],
+          [
+            -122.8118564,
+            49.28362
+          ],
+          [
+            -122.8107024,
+            49.2840364
+          ],
+          [
+            -122.8104854,
+            49.2841082
+          ],
+          [
+            -122.8102056,
+            49.2842048
+          ],
+          [
+            -122.8097485,
+            49.2843249
+          ]
+        ]
+      },
+      "id": "way/494682490"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/494682491",
+        "attribution": "GeoBase®",
+        "cycleway:right": "lane",
+        "geobase:acquisitionTechnique": "GPS",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "source": "Geobase_Import_2009"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8129194,
+            49.2833852
+          ],
+          [
+            -122.8128072,
+            49.283403
+          ]
+        ]
+      },
+      "id": "way/494682491"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146181",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8095441,
+            49.2844823
+          ],
+          [
+            -122.8096825,
+            49.28445
+          ]
+        ]
+      },
+      "id": "way/1212146181"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146182",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "1",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8096825,
+            49.28445
+          ],
+          [
+            -122.8098045,
+            49.2844209
+          ]
+        ]
+      },
+      "id": "way/1212146182"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146183",
+        "cycleway:right": "lane",
+        "cycleway:right:lane": "exclusive",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.809626,
+            49.2843462
+          ],
+          [
+            -122.8094825,
+            49.2843712
+          ]
+        ]
+      },
+      "id": "way/1212146183"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146185",
+        "cycleway:right": "lane",
+        "cycleway:right:lane": "exclusive",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8097485,
+            49.2843249
+          ],
+          [
+            -122.809626,
+            49.2843462
+          ]
+        ]
+      },
+      "id": "way/1212146185"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146186",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8128286,
+            49.2835095
+          ],
+          [
+            -122.8129597,
+            49.2834877
+          ]
+        ]
+      },
+      "id": "way/1212146186"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146187",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.818339,
+            49.2845344
+          ],
+          [
+            -122.8184528,
+            49.2845769
+          ]
+        ]
+      },
+      "id": "way/1212146187"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146188",
+        "cycleway:right": "lane",
+        "cycleway:right:lane": "exclusive",
+        "highway": "secondary",
+        "lanes": "2",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8185338,
+            49.2844888
+          ],
+          [
+            -122.818421,
+            49.2844467
+          ]
+        ]
+      },
+      "id": "way/1212146188"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146189",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8186318,
+            49.2845178
+          ],
+          [
+            -122.8185338,
+            49.2844888
+          ]
+        ]
+      },
+      "id": "way/1212146189"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1212146190",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.821594,
+            49.2844939
+          ],
+          [
+            -122.8213508,
+            49.2845809
+          ],
+          [
+            -122.8211708,
+            49.284633
+          ]
+        ]
+      },
+      "id": "way/1212146190"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1224005734",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.821171,
+            49.2847399
+          ],
+          [
+            -122.8214382,
+            49.2846612
+          ],
+          [
+            -122.821681,
+            49.2845673
+          ]
+        ]
+      },
+      "id": "way/1224005734"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1224005735",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "1",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8127365,
+            49.2835244
+          ],
+          [
+            -122.8128286,
+            49.2835095
+          ]
+        ]
+      },
+      "id": "way/1224005735"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1224005736",
+        "cycleway:right": "lane",
+        "highway": "secondary",
+        "lanes": "2",
+        "maxspeed": "50",
+        "name": "Guildford Way",
+        "oneway": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8184528,
+            49.2845769
+          ],
+          [
+            -122.8185477,
+            49.2846147
+          ]
+        ]
+      },
+      "id": "way/1224005736"
+    }
+  ]
+}
