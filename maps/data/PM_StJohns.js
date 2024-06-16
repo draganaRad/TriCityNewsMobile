4bcdd4b6ca4d75823ed00d1d223f4865cf50304c
Dragana Radulovic <dradulovic@Draganas-MBP.vc.shawcable.net>
2024-06-16 14:22:39 -0700

    added St Johns MUP

---
+++ maps/data/PM_StJohns.js
@@ -0,0 +1,392 @@
+var geojsonFeature = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2024-06-16T21:05:15Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/975488054",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "cycleway",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.866656,
+            49.2765286
+          ],
+          [
+            -122.8660552,
+            49.2765253
+          ],
+          [
+            -122.8656386,
+            49.2765238
+          ],
+          [
+            -122.8646759,
+            49.2765348
+          ],
+          [
+            -122.8642316,
+            49.2765428
+          ],
+          [
+            -122.8641465,
+            49.2765443
+          ],
+          [
+            -122.8641096,
+            49.276545
+          ],
+          [
+            -122.8629068,
+            49.276561
+          ],
+          [
+            -122.8618554,
+            49.2765764
+          ],
+          [
+            -122.8618276,
+            49.2765768
+          ],
+          [
+            -122.8617432,
+            49.2765782
+          ],
+          [
+            -122.8610703,
+            49.2765827
+          ],
+          [
+            -122.8607549,
+            49.2765882
+          ],
+          [
+            -122.8599796,
+            49.2766018
+          ],
+          [
+            -122.859561,
+            49.2766091
+          ],
+          [
+            -122.8587332,
+            49.2766196
+          ]
+        ]
+      },
+      "id": "way/975488054"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/975488056",
+        "bicycle": "designated",
+        "cycleway:surface": "asphalt",
+        "foot": "designated",
+        "footway:surface": "concrete",
+        "highway": "cycleway",
+        "segregated": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8564524,
+            49.276622
+          ],
+          [
+            -122.856127,
+            49.2766283
+          ],
+          [
+            -122.8554634,
+            49.276631
+          ],
+          [
+            -122.8548377,
+            49.2766278
+          ],
+          [
+            -122.8547986,
+            49.2766125
+          ],
+          [
+            -122.8547871,
+            49.2765985
+          ]
+        ]
+      },
+      "id": "way/975488056"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/975488087",
+        "bicycle": "designated",
+        "cycleway:surface": "asphalt",
+        "foot": "designated",
+        "footway:surface": "concrete",
+        "highway": "cycleway",
+        "segregated": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8545648,
+            49.276601
+          ],
+          [
+            -122.8545628,
+            49.2766054
+          ],
+          [
+            -122.8545042,
+            49.2766297
+          ],
+          [
+            -122.8543937,
+            49.2766258
+          ],
+          [
+            -122.8535167,
+            49.2766252
+          ],
+          [
+            -122.8527571,
+            49.2766201
+          ],
+          [
+            -122.8526173,
+            49.2766175
+          ],
+          [
+            -122.8525573,
+            49.2765875
+          ]
+        ]
+      },
+      "id": "way/975488087"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/975781610",
+        "bicycle": "designated",
+        "crossing:island": "no",
+        "foot": "designated",
+        "highway": "cycleway",
+        "segregated": "yes",
+        "tactile_paving": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8547871,
+            49.2765985
+          ],
+          [
+            -122.854646,
+            49.2766001
+          ],
+          [
+            -122.8545648,
+            49.276601
+          ]
+        ]
+      },
+      "id": "way/975781610"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1009364987",
+        "bicycle": "designated",
+        "cycleway:surface": "asphalt",
+        "foot": "designated",
+        "footway:surface": "concrete",
+        "highway": "cycleway",
+        "segregated": "yes"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8585932,
+            49.2766187
+          ],
+          [
+            -122.8579504,
+            49.2766175
+          ],
+          [
+            -122.8576113,
+            49.2766169
+          ],
+          [
+            -122.857549,
+            49.2766165
+          ],
+          [
+            -122.8571561,
+            49.2766143
+          ],
+          [
+            -122.8567402,
+            49.2766137
+          ],
+          [
+            -122.8567263,
+            49.2766214
+          ]
+        ]
+      },
+      "id": "way/1009364987"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1009364988",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "cycleway",
+        "segregated": "no",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8587332,
+            49.2766196
+          ],
+          [
+            -122.858663,
+            49.2766191
+          ],
+          [
+            -122.8585932,
+            49.2766187
+          ]
+        ]
+      },
+      "id": "way/1009364988"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1241214033",
+        "bicycle": "designated",
+        "crossing:island": "no",
+        "crossing:markings": "yes",
+        "foot": "designated",
+        "highway": "cycleway",
+        "segregated": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.852325,
+            49.2765843
+          ],
+          [
+            -122.852425,
+            49.2765857
+          ],
+          [
+            -122.8525573,
+            49.2765875
+          ]
+        ]
+      },
+      "id": "way/1241214033"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1288999024",
+        "bicycle": "designated",
+        "foot": "designated",
+        "highway": "cycleway",
+        "segregated": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.852325,
+            49.2765843
+          ],
+          [
+            -122.8522224,
+            49.2765967
+          ],
+          [
+            -122.8516153,
+            49.2765912
+          ],
+          [
+            -122.8513528,
+            49.2765745
+          ],
+          [
+            -122.8507207,
+            49.2765672
+          ],
+          [
+            -122.850435,
+            49.2765723
+          ],
+          [
+            -122.8502831,
+            49.2765691
+          ]
+        ]
+      },
+      "id": "way/1288999024"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/1288999028",
+        "bicycle": "designated",
+        "crossing:markings": "yes",
+        "foot": "designated",
+        "highway": "cycleway",
+        "segregated": "yes",
+        "surface": "asphalt"
+      },
+      "geometry": {
+        "type": "LineString",
+        "coordinates": [
+          [
+            -122.8564524,
+            49.276622
+          ],
+          [
+            -122.8565987,
+            49.2766202
+          ],
+          [
+            -122.8567263,
+            49.2766214
+          ]
+        ]
+      },
+      "id": "way/1288999028"
+    }
+  ]
+}
